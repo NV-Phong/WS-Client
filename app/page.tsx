3f554698da8025cde2d4a745f2df6f664f56b02a
NV-Phong <0h9ng03@gmail.com>
2025-03-28 03:01:37 +0700

    FEAT - landing page first look

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,58 @@
-import Image from "next/image";
+"use client";
+import GradualSpacing from "@/components/ui/gradual-spacing";
+import Particles from "@/components/ui/particles";
+// import { ModeToggle } from "@/components/ui/mode-toggle";
+import { Spotlight } from "@/components/ui/spotlight-new";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-   return (
-      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-         <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-            <Image
-               className="dark:invert"
-               src="/next.svg"
-               alt="Next.js logo"
-               width={180}
-               height={38}
-               priority
-            />
-            <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-               <li className="mb-2 tracking-[-.01em]">
-                  Get started by editing{" "}
-                  <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-                     app/page.tsx
-                  </code>
-                  .
-               </li>
-               <li className="tracking-[-.01em]">
-                  Save and see your changes instantly.
-               </li>
-            </ol>
+   const { resolvedTheme } = useTheme();
+   const [color, setColor] = useState("#ffffff");
 
-            <div className="flex gap-4 items-center flex-col sm:flex-row">
-               <a
-                  className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-                  href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                  target="_blank"
-                  rel="noopener noreferrer"
-               >
-                  <Image
-                     className="dark:invert"
-                     src="/vercel.svg"
-                     alt="Vercel logomark"
-                     width={20}
-                     height={20}
-                  />
-                  Deploy now
-               </a>
-               <a
-                  className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-                  href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                  target="_blank"
-                  rel="noopener noreferrer"
-               >
-                  Read our docs
-               </a>
+   useEffect(() => {
+      setColor(resolvedTheme === "dark" ? "#ffffff" : "#000000");
+   }, [resolvedTheme]);
+
+   return (
+      <div className="">
+         <div className="flex items-center justify-center z-0 h-screen w-full rounded-md bg-background antialiased bg-grid-white/[0.02] relative overflow-hidden">
+            <Spotlight
+               // className="-top-40 left-0 md:left-60 md:-top-20"
+               // fill="white"
+            />
+             {/* <ModeToggle /> */}
+
+            <div className="flex flex-col items-center justify-center">
+               <div className="mb-9 max-w-7xl mx-auto relative z-10 w-full pt-20 md:pt-0 text-center">
+                  <h1 className="h-40 text-xl md:text-5xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-400 to-neutral-900 bg-opacity-50">
+                     Hi there ! I&apos;m
+                     <GradualSpacing
+                        className="font-display text-center text-5xl font-bold -tracking-widest  text-black dark:text-white md:text-7xl md:leading-[5rem]"
+                        text="WorkSpace"
+                     />
+                     <br />
+                  </h1>
+                  <p className="font-normal text-base dark:text-primary/60 light:text-neutral-900 max-w-3xl mx-auto">
+                     I&apos;m on a journey to become a Full-Stack developer.
+                     With my knowledge of UI/UX design. I enjoy solving
+                     Front-End problems and creating the best user experience
+                     for users. I also spend time learning new technologies and
+                     best practices to become a better engineer. I like to learn
+                     about process automation or applying AI to improve work
+                     productivity.
+                  </p>
+               </div>
             </div>
-         </main>
-         <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-            <a
-               className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-               href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-               target="_blank"
-               rel="noopener noreferrer"
-            >
-               <Image
-                  aria-hidden
-                  src="/file.svg"
-                  alt="File icon"
-                  width={16}
-                  height={16}
-               />
-               Learn
-            </a>
-            <a
-               className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-               href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-               target="_blank"
-               rel="noopener noreferrer"
-            >
-               <Image
-                  aria-hidden
-                  src="/window.svg"
-                  alt="Window icon"
-                  width={16}
-                  height={16}
-               />
-               Examples
-            </a>
-            <a
-               className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-               href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-               target="_blank"
-               rel="noopener noreferrer"
-            >
-               <Image
-                  aria-hidden
-                  src="/globe.svg"
-                  alt="Globe icon"
-                  width={16}
-                  height={16}
-               />
-               Go to nextjs.org →
-            </a>
-         </footer>
+
+            <Particles
+               className="absolute inset-0"
+               quantity={100}
+               ease={80}
+               color={color}
+               refresh
+            />
+         </div>
       </div>
    );
 }
